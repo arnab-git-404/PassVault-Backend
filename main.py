1a8d2566a1e4a2b6362e12d46c4ca482f2164603
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-10-13 15:30:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://pass-vault-frontend.vercel.app"],
+    allow_origins=["https://pass-vault-frontend.vercel.app","*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
